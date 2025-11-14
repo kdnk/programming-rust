b17e4f91af94258dcd6b955448480c6054514403
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2025-11-14 22:41:46 +0900

    chore

--- c-05/src/main.rs
+++ c-05/src/main.rs
@@ -246,4 +246,22 @@ fn main() {
             s: S<'a>,
         }
     }
+
+    fn f15() {
+        struct S<'a, 'b> {
+            x: &'a i32,
+            y: &'b i32,
+        }
+
+        let x = 10;
+        let r;
+        {
+            let y = 20;
+            {
+                let s = S { x: &x, y: &y };
+                r = s.x;
+            }
+        }
+        println!("{}", r);
+    }
 }
