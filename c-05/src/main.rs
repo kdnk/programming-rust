4aff17315248ac1a2233012639162aea98bb9b56
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2025-11-16 14:49:07 +0900

    chore

--- c-05/src/main.rs
+++ c-05/src/main.rs
@@ -277,12 +277,12 @@ fn main() {
     }
 
     fn f5_4_0() {
-        {
-            let v = vec![4, 8, 19, 27, 34, 10];
-            let r = &v;
-            let aside = v;
-            r[0];
-        }
+        // {
+        //     let v = vec![4, 8, 19, 27, 34, 10];
+        //     let r = &v;
+        //     let aside = v;
+        //     r[0];
+        // }
         {
             let v = vec![4, 8, 19, 27, 34, 10];
             {
@@ -290,6 +290,24 @@ fn main() {
                 r[0];
             }
             let aside = v;
+        }
+        {
+            fn extend(vec: &mut Vec<f64>, slice: &[f64]) {
+                for elt in slice {
+                    vec.push(*elt);
+                }
+            }
+
+            let mut wave = Vec::new();
+            let head = vec![0.0, 1.0];
+            let tail = [0.0, -1.0];
+
+            extend(&mut wave, &head);
+            extend(&mut wave, &tail);
+
+            assert_eq!(wave, vec![0.0, 1.0, 0.0, -1.0]);
+
+            extend(&mut wave, &wave);
         }
     }
 }
