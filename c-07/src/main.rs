d046ec212f0369e36bc114dcbbf709488bf69bba
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2025-12-06 00:21:52 +0900

    chore: panic

--- c-07/src/main.rs
+++ c-07/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    fn f7_1_1() {
+        fn pirate_share(total: u64, crew_size: usize) -> u64 {
+            let half = total / 2;
+            half / crew_size as u64
+        }
+        pirate_share(10, 0);
+    }
+    f7_1_1()
 }
