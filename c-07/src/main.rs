d1c3ab59785f964076626295ecb45e3045b05865
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2025-12-07 22:47:16 +0900

    chore

--- c-07/src/main.rs
+++ c-07/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     fn f7_1_1() {
         fn pirate_share(total: u64, crew_size: usize) -> u64 {
@@ -6,5 +8,42 @@ fn main() {
         }
         pirate_share(10, 0);
     }
-    f7_1_1()
+    fn f7_2_1() {
+        #[derive(Debug)]
+        struct WetherReport<'a> {
+            condition: &'a str,
+            temp: u64,
+        }
+        fn get_weather(location: &'_ str) -> Result<WetherReport<'_>, io::Error> {
+            if location.is_empty() {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "location must not be empty",
+                ));
+            }
+            Ok(WetherReport {
+                condition: "Sunny",
+                temp: 20,
+            })
+        }
+
+        match get_weather(&"japan".to_string()) {
+            Ok(report) => {
+                println!("japan: {:?}", report);
+            }
+            Err(err) => {
+                print!("eror: {}", err);
+            }
+        }
+
+        const THE_USUAL: WetherReport = WetherReport {
+            condition: "Sunny",
+            temp: 22,
+        };
+
+        let report = get_weather("").unwrap_or(THE_USUAL);
+        println!("report: {:?}", report);
+    }
+
+    f7_2_1()
 }
