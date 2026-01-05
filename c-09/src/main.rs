8a3a68ce26506aac490f4e7783fe0417d79cb6cc
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2026-01-05 20:43:53 +0900

    wip(c-09)

--- c-09/src/main.rs
+++ c-09/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Broom {
+    name: String,
+    height: u32,
+    health: u32,
+    position: (f32, f32, f32),
+    intent: BroomIntent,
+}
+
+#[derive(Copy, Clone)]
+enum BroomIntent {
+    FetchWater,
+    DumpWater,
+}
+
+fn chop(b: Broom) -> (Broom, Broom) {
+    let mut boom1 = Broom {
+        height: b.height / 2,
+        ..b
+    };
+}
