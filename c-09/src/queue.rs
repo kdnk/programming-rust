48848e09b1528586964e21843c75928197db0c9b
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2026-01-15 21:25:22 +0900

    wip

--- c-09/src/queue.rs
+++ c-09/src/queue.rs
@@ -21,3 +21,49 @@ impl Queue {
         self.older.pop()
     }
 }
+
+struct Extrema<'elt> {
+    greatest: &'elt i32,
+    least: &'elt i32,
+}
+
+fn find_extrema<'s>(slice: &'s [i32]) -> Extrema<'s> {
+    let mut greatest = &slice[0];
+    let mut least = &slice[0];
+
+    for i in 1..slice.len() {
+        if slice[i] < *least {
+            least = &slice[i];
+        }
+        if slice[i] > *greatest {
+            greatest = &slice[i];
+        }
+    }
+
+    Extrema { greatest, least }
+}
+
+fn main() {
+    let a = [0, -3, 0, 15, 48];
+    let e = find_extrema(&a);
+    assert_eq!(*e.least, -3);
+    assert_eq!(*e.greatest, 48);
+}
+
+struct Polynomial<const N: usize> {
+    coefficients: [f64; N],
+}
+
+impl<const N: usize> Polynomial<N> {
+    fn new(coefficients: [f64; N]) -> Polynomial<N> {
+        Polynomial { coefficients }
+    }
+
+    fn eval(&self, x: f64) -> f64 {
+        let mut sum = 0.0;
+        for i in (0..N).rev() {
+            sum = self.coefficients[i] + x * sum;
+        }
+        sum
+    }
+}
